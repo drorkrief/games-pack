4cedf9b90c985c7b776a1cc21d3ec01a4478c833
dror <dror@ardon.co.il>
2022-04-04 14:35:01 +0300

    some fixes + new game

--- src/Memory Game/Card.js
+++ src/Memory Game/Card.js
@@ -1,22 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-function Card({item}) {
-    const [flip,setflip] = useState(false);
+function Card({ item, clickHandle }) {
+//   const [flip, setflip] = useState(false);
   return (
-    <div className={flip?'flip-card flipnow':'flip-card'} onClick={()=> setflip(prev=>!prev)}>
-         <div className={'flip-card-inner'}>
-    <div className="flip-card-front">
-      {/* <img src="img_avatar.png" alt="Avatar" style={{width:"300px",height:"300px"}}/> */}
-    <img src={process.env.PUBLIC_URL +item.img}/>
+    <div
+      className={item.current === "check"? "flip-card" : "flip-card"}
+      onClick={() => clickHandle(item.id)}
+    >
+      <div className={item.current ? "flip-card-inner flipnow" : "flip-card-inner"}>
+        <div className="flip-card-front">
+          {/* <img src="img_avatar.png" alt="Avatar" style={{width:"300px",height:"300px"}}/> */}
+          <h1>?</h1>
+        </div>
+        <div className="flip-card-back">
+          <img src={process.env.PUBLIC_URL + item.img} />
+        </div>
+      </div>
     </div>
-    <div class="flip-card-back">
-      <h1>John Doe</h1> 
-      <p>Architect & Engineer</p> 
-      <p>We love that guy</p>
-    </div>
-  </div>
-    </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
--- src/Memory Game/MemoryGame.js
+++ src/Memory Game/MemoryGame.js
@@ -5,39 +5,64 @@ import "./MemoryGame.css";
 export const MemoryGame = () => {
   const [cards, setCards] = useState(
     [
-      { id: 1, img: "/images/Duck.png", current: "" },
-      { id: 1, img: "/images/Duck.png", current: "" },
-      { id: 2, img: "/images/Chick.png", current: "" },
-      { id: 2, img: "/images/Chick.png", current: "" },
-      { id: 3, img: "/images/Crocodile.png", current: "" },
-      { id: 3, img: "/images/Crocodile.png", current: "" },
-      { id: 4, img: "/images/cat.png", current: "" },
-      { id: 4, img: "/images/cat.png", current: "" },
-      { id: 5, img: "/images/dog.png", current: "" },
-      { id: 5, img: "/images/dog.png", current: "" },
-      { id: 6, img: "/images/Fox.png", current: "" },
-      { id: 6, img: "/images/Fox.png", current: "" },
-      { id: 7, img: "/images/Horse.png", current: "" },
-      { id: 7, img: "/images/Horse.png", current: "" },
-      { id: 8, img: "/images/Koala.png", current: "" },
-      { id: 8, img: "/images/Koala.png", current: "" },
-      { id: 9, img: "/images/Penguin.png", current: "" },
-      { id: 9, img: "/images/Penguin.png", current: "" },
-      { id: 10, img: "/images/Wolf.png", current: "" },
-      { id: 10, img: "/images/Wolf.png", current: "" },
+      { id: 1, group: 1, img: "/images/Duck.png", current: "" },
+      { id: 2, group: 1, img: "/images/Duck.png", current: "" },
+      { id: 3, group: 2, img: "/images/Chick.png", current: "" },
+      { id: 4, group: 2, img: "/images/Chick.png", current: "" },
+      { id: 5, group: 3, img: "/images/Crocodile.png", current: "" },
+      { id: 6, group: 3, img: "/images/Crocodile.png", current: "" },
+      { id: 7, group: 4, img: "/images/cat.png", current: "" },
+      { id: 8, group: 4, img: "/images/cat.png", current: "" },
+      { id: 9, group: 5, img: "/images/dog.png", current: "" },
+      { id: 10, group: 5, img: "/images/dog.png", current: "" },
+      { id: 11, group: 6, img: "/images/Fox.png", current: "" },
+      { id: 12, group: 6, img: "/images/Fox.png", current: "" },
+      { id: 13, group: 7, img: "/images/Horse.png", current: "" },
+      { id: 14, group: 7, img: "/images/Horse.png", current: "" },
+      { id: 15, group: 8, img: "/images/Koala.png", current: "" },
+      { id: 16, group: 8, img: "/images/Koala.png", current: "" },
+      { id: 17, group: 9, img: "/images/Penguin.png", current: "" },
+      { id: 18, group: 9, img: "/images/Penguin.png", current: "" },
+      { id: 19, group: 10, img: "/images/Wolf.png", current: "" },
+      { id: 20, group: 10, img: "/images/Wolf.png", current: "" },
     ].sort(() => (Math.random() > 0.5 ? 1 : -1))
   );
+  const clickHandle = (parameter) => {
+    let temp = [...cards];
+    let tempId = temp.findIndex((obj) => obj.id === parameter);
+    if (temp[tempId].current !== "") {
+      return;
+    }
+    let active = temp.findIndex((obj) => obj.current === "check");
+    if (active !== -1) {
+      console.log(active, "active");
+      console.log(tempId,"tempId");
+      if (temp[tempId].group === temp[active].group) {
+        console.log("ok");
+        temp[tempId].current = "fit";
+        temp[active].current = "fit";
+      } else {
+        console.log("no");
+        temp[tempId].current = "check";
+      }
+    } else{
+
+        temp[tempId].current = "check";
+    }
+    console.log(tempId);
+    setCards(temp);
+    //   console.log(parameter);
+  };
   return (
     <>
       <div>MemoryGame</div>
       <div className="mamoContainer">
-
-      {cards.map((item, index) => (
-          <div  key={`key${index}`}>
-            <Card item={item}/>
-          {/* <img src={process.env.PUBLIC_URL +item.img} alt="pic" width="90px"/> */}
-        </div>
-      ))}
+        {cards?.map((item, index) => (
+          <div key={`key${index}`}>
+            <Card clickHandle={clickHandle} item={item} />
+            {/* <img src={process.env.PUBLIC_URL +item.img} alt="pic" width="90px"/> */}
+          </div>
+        ))}
       </div>
     </>
   );
