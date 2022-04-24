7ab1d072246988910e0640fcce795a1267daeb3f
dror <dror@ardon.co.il>
2022-04-24 16:50:12 +0300

    test

--- src/Memory Game/Card.js
+++ src/Memory Game/Card.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-export const Card = ({ cardprops, clickHandle }) => {
+export const Card = ({ cardprops, clickHandle, resetCardStatus }) => {
+  useEffect(()=>{
+    const timer = setTimeout(() => {
+      if(cardprops.statusCard === "noFit"){
+        // console.log(cardprops.name);
+        resetCardStatus(cardprops.id);
+      }
+      // console.log('This will run after 1 second!')
+    }, 1000);
+    return () => clearTimeout(timer);
+    
+  },[cardprops.statusCard]);
   return (
     <div>
       <div className="flip-card" onClick={() => clickHandle(cardprops.id)}>
@@ -12,7 +23,7 @@ export const Card = ({ cardprops, clickHandle }) => {
           <div className="flip-card-front">
             <h1 style={{ fontSize: "5vw" }}>?</h1>
           </div>
-          <div className="flip-card-back">
+          <div className={cardprops.statusCard === "noFit" ?`flip-card-back noFit`:cardprops.statusCard === "fit" ?`flip-card-back fit` :"flip-card-back"}>
             <img
               src={process.env.PUBLIC_URL + `/images/${cardprops.name}.png`}
               alt={cardprops.name}
--- src/Memory Game/MemoryCardsGame.js
+++ src/Memory Game/MemoryCardsGame.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 import { Card } from "./Card";
 import "./MemoryCardsGame.css";
 export const MemoryCardsGame = () => {
-  const [clicked, setlicked] = useState();
-  const [check, setCheck] = useState();
+  const [clicked, setlicked] = useState(0);
+  // const [check, setCheck] = useState();
+
   let cardsList = [
     { id: 1, name: "Duck", statusCard: "" },
     { id: 2, name: "Duck", statusCard: "" },
@@ -48,19 +49,42 @@ export const MemoryCardsGame = () => {
     // console.log(array);
     return array;
   }
+  const resetCardStatus = useCallback((cardId) => {
+    let newArray = [...cardListS];
+    let temp = newArray.findIndex(x => x.id === cardId)
+    console.log(temp);
+    newArray[temp].statusCard = "";
+    setCardList(newArray);
+    setlicked(clicked + 1);
+
+  },[cardListS])
   const clickHandle = (cardId) => {
     let newArray = [...cardListS];
     let isCheck = newArray.find((x) => x.statusCard === "check");
     var arrIndex = newArray.findIndex((x) => x.id === cardId);
-    console.log(isCheck);
+    // console.log(isCheck);
     if (cardListS[arrIndex].statusCard) {
       console.log("is clicked");
       return
     }
     if (isCheck) { // there is check before;
-      console.log("123123");
+      console.log(isCheck.id ,"is in check mode");
+      let tempIndex = newArray.findIndex((x) => x.statusCard === "check");
+      if (isCheck.name === newArray[arrIndex].name) {
+        console.log("ok");
+        newArray[tempIndex].statusCard = "fit";
+        newArray[arrIndex].statusCard = "fit";
+        // newArray[isCheck.id].statusCard = "fit";
+        setCardList(newArray);
+      }else{
+        console.log("no");
+        newArray[tempIndex].statusCard = "noFit";
+        newArray[arrIndex].statusCard = "noFit";
+        setCardList(newArray);
+
+      }
     } else{
-      console.log("--");
+      console.log("no one is in check mode");
       
       newArray[arrIndex].statusCard = "check";
       setCardList(newArray);
@@ -85,11 +109,12 @@ export const MemoryCardsGame = () => {
   return (
     <>
       <h1>MemoryCardsGame</h1>
+      <h3>you're failed {clicked} times</h3>
       <div className="cardsWrapper">
         {cardListS.map((card, index) => (
-          <div key={index}>
+          <div key={`cardNum${index}`}>
             {/* {card} */}
-            <Card cardprops={card} clickHandle={clickHandle} />
+            <Card cardprops={card} clickHandle={clickHandle} resetCardStatus={resetCardStatus} />
           </div>
         ))}
       </div>
