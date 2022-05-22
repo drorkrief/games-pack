3a8a00fb5f989b2172015ac5b07762def109d67b
dror <dror@ardon.co.il>
2022-05-22 15:49:41 +0300

    open graph pic and title

--- src/App.js
+++ src/App.js
@@ -5,38 +5,68 @@ import { MemoryCardsGame } from "./Memory Game/MemoryCardsGame";
 import { TicTacToe } from "./TicTacToe/TicTacToe";
 import { NoMatch } from "./Memory Game/NoMatch";
 import { Home } from "./Memory Game/Home";
-
+import { useEffect, useState } from "react";
+import Helmet from "react-helmet";
 function App() {
+  const [dataYnet, setdataYnet] = useState(false);
+  useEffect(() => {
+    fetch("https://www.ynet.co.il/iphone/json/api/auto_ticker/6QMCMRP1V")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then(data =>{
+        setdataYnet(data.data[1])
+      })
+      .catch((err) => {
+        setdataYnet(err);
+      })
+      .finally(() => {
+        console.log("ok!");
+      });
+  }, []);
+  console.log(dataYnet);
   return (
     <HashRouter basename="/">
-    <div className="App">
-      <main
-        style={{
-          display: "flex",
-          gap: "2vw",
-          justifyContent: "start",
-          background: "silver",
-          lineHeight: "30px",
-          paddingLeft: "20px",
-        }}
-      >
-        <Link to="/">Home</Link>
-        <Link to="/tictactoe">TicTacToe</Link>
 
-        <Link to="/memorycardsgame">MemoryCardsGame</Link>
-      </main>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/tictactoe" element={<TicTacToe />} />
-        <Route exact path="/memorycardsgame" element={<MemoryCardsGame />} />
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
-      {/* <h1>Tic Tac Toe</h1>
+      <div className="App">
+      <Helmet><title>adf</title>
+        {dataYnet&&
+    <title>{dataYnet.title}</title>}
+    {dataYnet&&<meta property="og:title" content={dataYnet.title} />}
+    {dataYnet&&<meta property="og:description" content={dataYnet.textToShow} />}
+    {dataYnet&&<meta property="og:image" content={dataYnet.promotionImageDetails.url} />}
+ </Helmet>
+        <main
+          style={{
+            display: "flex",
+            gap: "2vw",
+            justifyContent: "start",
+            background: "silver",
+            lineHeight: "30px",
+            paddingLeft: "20px",
+          }}
+        >
+          <Link to="/">Home</Link>
+          <Link to="/tictactoe">TicTacToe</Link>
+
+          <Link to="/memorycardsgame">MemoryCardsGame</Link>
+        </main>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/tictactoe" element={<TicTacToe />} />
+          <Route exact path="/memorycardsgame" element={<MemoryCardsGame />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+        {/* <h1>Tic Tac Toe</h1>
     <TicTacToe/> */}
-      {/* <hr/> */}
-      {/* <MemoryGame/> */}
-      {/* <MemoryCardsGame/> */}
-    </div></HashRouter>
+        {/* <hr/> */}
+        {/* <MemoryGame/> */}
+        {/* <MemoryCardsGame/> */}
+      </div>
+    </HashRouter>
   );
 }
 
